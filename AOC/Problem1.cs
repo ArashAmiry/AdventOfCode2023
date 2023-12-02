0a635c6fe46483b172e451a6f505f19340740ae4
Arash Amiry <ArashAmiry@hotmail.com>
2023-12-02 15:59:25 +0100

    Problem 1 Solution 1

---
+++ AOC/Problem1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC
+{
+    public class Problem1
+    {
+        private double total = 0;
+        private string firstNumber;
+        private string secondNumber;
+
+        public double Solution1(string filePath)
+        {
+            string[] lines = File.ReadAllLines(filePath);
+
+            foreach (string line in lines)
+            {
+                foreach (char c in line)
+                {
+                    if (Char.IsNumber(c))
+                    {
+                        firstNumber = Convert.ToString(Char.GetNumericValue(c));
+                        break;
+                    }
+                }
+
+                var reversedLine = ReverseString(line);
+
+                foreach (char c in reversedLine)
+                {
+                    if (Char.IsNumber(c))
+                    {
+                        secondNumber = Convert.ToString(Char.GetNumericValue(c));
+                        break;
+                    }
+                }
+
+                string joinNumber = firstNumber + secondNumber;
+                total += Int32.Parse(joinNumber);
+            }
+
+            return total;
+        }
+
+        private string ReverseString(string line)
+        {
+            char[] charArray = line.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
---
+++ AOC/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Problem1 problem = new Problem1();
+
+            Console.WriteLine(problem.Solution1("C:\\Users\\Arash\\Desktop\\AOC2023\\AOC\\Inputs\\input1.txt"));
+        }
+    }
+}
